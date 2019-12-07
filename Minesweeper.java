028702cb7c178dd993c19d73384e76d8a2aa74c2
laogao216 <57880087+laogao216@users.noreply.github.com>
2019-12-07 05:11:48 -0600

    version 1.2
     - minor bug fix
     - readability improvement

--- Minesweeper.java
+++ Minesweeper.java
@@ -20,10 +20,13 @@ public class Minesweeper {
   private PImage triggeredMine;
   private PImage blank;
   private int[][] key = new int[Driver.row][Driver.col];
+  // represents game board. Value: 1 ~ 8 for normal squares, 9 for mine squares
   private String[][] state = new String[Driver.row][Driver.col];
+  // represents game progress. controls what is displayed on each square
+  private boolean gameOver = false;
   // TODO - for future developments: add mines left, message, and new game button
   // private final static String ALIVE = "alive ^-^";
-  // private final static String DEAD = "dead *~*";
+  // private final static String gameOver = "gameOver *~*";
   // private final static String WIN = "victory ^o^";
 
   /**
@@ -33,22 +36,7 @@ public class Minesweeper {
    */
   public Minesweeper(PApplet processing) {
     this.processing = processing;
-    initGame();
-  }
-
-  /**
-   * initializes the game setup
-   */
-  private void initGame() {
-    loadImages();
-    genKey();
-    initState();
-  }
-
-  /**
-   * load 16*16 images that represent a single square to private PImage fields
-   */
-  private void loadImages() {
+    // load images
     for (int i = 0; i < 9; i++) {
       String name = "images" + File.separator + Integer.toString(i) + ".png";
       PImage image = processing.loadImage(name);
@@ -59,12 +47,7 @@ public class Minesweeper {
     mine = processing.loadImage("images" + File.separator + "mine.png");
     triggeredMine = processing.loadImage("images" + File.separator + "triggered_mine.png");
     blank = processing.loadImage("images" + File.separator + "blank.png");
-  }
-
-  /**
-   * randomize mine locations and generate a number map around mines, stored in int array key
-   */
-  private void genKey() {
+    // populate key
     boolean[][] seatAvailable = new boolean[Driver.row][Driver.col];
     for (int row = 0; row < Driver.row; row++) {
       for (int col = 0; col < Driver.col; col++) {
@@ -72,8 +55,15 @@ public class Minesweeper {
       }
     }
     for (int i = 0; i < Driver.mineCount; i++) {
-      int row = randInt(Driver.row);
-      int col = randInt(Driver.row);
+      Random random = new Random();
+      int row = random.nextInt() % Driver.row;
+      if (row < 0) {
+        row = row * -1;
+      }
+      int col = random.nextInt() % Driver.row;
+      if (col < 0) {
+        col = col * -1;
+      }
       if (seatAvailable[row][col] == true) {
         key[row][col] = 9;
         seatAvailable[row][col] = false;
@@ -85,168 +75,100 @@ public class Minesweeper {
       for (int col = 0; col < Driver.col; col++) {
         if (key[row][col] != 9) {
           int count = 0;
-          if (row > 0 && col > 0 && key[row - 1][col - 1] == 9)
-            count += 1;
-          if (row > 0 && key[row - 1][col] == 9)
-            count += 1;
-          if (row > 0 && col < Driver.col - 1 && key[row - 1][col + 1] == 9)
-            count += 1;
-          if (col < Driver.col - 1 && key[row][col + 1] == 9)
-            count += 1;
-          if (row < Driver.row - 1 && col < Driver.col - 1 && key[row + 1][col + 1] == 9)
-            count += 1;
-          if (row < Driver.row - 1 && key[row + 1][col] == 9)
-            count += 1;
-          if (row < Driver.row - 1 && col > 0 && key[row + 1][col - 1] == 9)
-            count += 1;
-          if (col > 0 && key[row][col - 1] == 9)
-            count += 1;
+          int neighbor[][] = neighbors(row, col);
+          for (int i = 0; i < 8; i++) {
+            if (neighbor[i][0] != -1 && key[neighbor[i][0]][neighbor[i][1]] == 9) {
+              count += 1;
+            }
+          }
           key[row][col] = count;
         }
       }
     }
-  }
-
-  /**
-   * generate a random int between 0 (inclusive) and bound (exclusive)
-   * 
-   * @param bound - the int that the max return value is one fewer
-   * 
-   * @return random int between 0 (inclusive) and bound (exclusive)
-   */
-  private int randInt(int bound) {
-    Random random = new Random();
-    int output = random.nextInt() % bound;
-    if (output < 0)
-      output = output * -1;
-    return output;
-  }
-
-  /**
-   * initialize String array state[row][col] over every individual square to "covered"
-   */
-  private void initState() {
+    // initialize state to "covered" for each square
     for (int row = 0; row < Driver.row; row++) {
       for (int col = 0; col < Driver.col; col++) {
         state[row][col] = "covered";
       }
     }
+    // draw the initialized game, ready for player action
+    draw();
   }
 
   /**
    * called by Driver.draw continuously. process user inputs by altering the state of each square
    * 
-   * @param mouseX - x coordinate of location of the mouse
-   * 
-   * @param mouseY - y coordinate of location of the mouse
-   * 
+   * @param mouseX       - x coordinate of location of the mouse
+   * @param mouseY       - y coordinate of location of the mouse
    * @param mousePressed - true is mouse is pressed, false if otherwise
-   * 
-   * @param mouseButton - the button on the mouse that is pressed: left = 37, center = 3, right = 39
+   * @param mouseButton  - the button on the mouse that is pressed: left = 37, center = 3, right =
+   *                     39
    */
   public void update(int mouseX, int mouseY, boolean mousePressed, int mouseButton) {
-    // FIXME - restart game if first move kills
-    // TODO - for future developments: if covered = mines left, flag all
-    if (mousePressed == true && mouseX > 0 && mouseX < Driver.row * 16 + 1 && mouseY > 0
-        && mouseY < Driver.col * 16 + 1) {
-      if (key[(mouseX - 1) / 16][(mouseY - 1) / 16] == 9 && mouseButton == 37
-          && state[(mouseX - 1) / 16][(mouseY - 1) / 16].equals("covered")) {
-        state[(mouseX - 1) / 16][(mouseY - 1) / 16] = "triggeredMine";
-        for (int row = 0; row < Driver.row; row++) {
-          for (int col = 0; col < Driver.col; col++) {
-            if (state[row][col].equals("flagged") && key[row][col] != 9) {
-              state[row][col] = "badFlag";
-            } else if (state[row][col].equals("flagged") == false
-                && state[row][col].equals("triggeredMine") == false) {
-              state[row][col] = "uncovered";
+    // TODO - if covered = mines left, flag all
+    if (gameOver == false && mousePressed == true && mouseX > 0 && mouseX < Driver.row * 16 + 1
+        && mouseY > 0 && mouseY < Driver.col * 16 + 1) {
+      // TODO - restart game if first move does not have key == 0
+      int row = (mouseX - 1) / 16;
+      int col = (mouseY - 1) / 16;
+      int neighbor[][] = neighbors(row, col);
+      if (key[row][col] == 9 && mouseButton == 37 && state[row][col].equals("covered")) {
+        gameOver = true;
+        state[row][col] = "triggeredMine";
+        for (int r = 0; r < Driver.row; r++) {
+          for (int c = 0; c < Driver.col; c++) {
+            if (state[r][c].equals("flagged") && key[r][c] != 9) {
+              state[r][c] = "badFlag";
+            } else if (state[r][c].equals("flagged") == false
+                && state[r][c].equals("triggeredMine") == false) {
+              state[r][c] = "uncovered";
             }
           }
         }
       }
-      if (key[(mouseX - 1) / 16][(mouseY - 1) / 16] != 9 && mouseButton == 37
-          && state[(mouseX - 1) / 16][(mouseY - 1) / 16].equals("flagged") == false) {
-        state[(mouseX - 1) / 16][(mouseY - 1) / 16] = "uncovered";
+      if (key[row][col] != 9 && mouseButton == 37 && state[row][col].equals("flagged") == false) {
+        state[row][col] = "uncovered";
         // FIXME - uncover continuously with a recursive
       }
-      if (state[(mouseX - 1) / 16][(mouseY - 1) / 16].equals("covered") && mouseButton == 39) {
-        state[(mouseX - 1) / 16][(mouseY - 1) / 16] = "flagged";
+      if (state[row][col].equals("covered") && mouseButton == 39) {
+        state[row][col] = "flagged";
       }
-      if (state[(mouseX - 1) / 16][(mouseY - 1) / 16].equals("flagged") && mouseButton == 3) {
-        state[(mouseX - 1) / 16][(mouseY - 1) / 16] = "covered";
+      if (state[row][col].equals("flagged") && mouseButton == 3) {
+        state[row][col] = "covered";
       }
-      if (state[(mouseX - 1) / 16][(mouseY - 1) / 16].equals("uncovered") && mouseButton == 3) {
-        int row = (mouseX - 1) / 16;
-        int col = (mouseY - 1) / 16;
-        int key = this.key[row][col];
+      if (state[row][col].equals("uncovered") && mouseButton == 3) {
         int count = 0;
-        if (row > 0 && col > 0 && state[row - 1][col - 1].equals("flagged"))
-          count += 1;
-        if (row > 0 && state[row - 1][col].equals("flagged"))
-          count += 1;
-        if (row > 0 && col < Driver.col - 1 && state[row - 1][col + 1].equals("flagged"))
-          count += 1;
-        if (col < Driver.col - 1 && state[row][col + 1].equals("flagged"))
-          count += 1;
-        if (row < Driver.row - 1 && col < Driver.col - 1
-            && state[row + 1][col + 1].equals("flagged"))
-          count += 1;
-        if (row < Driver.row - 1 && state[row + 1][col].equals("flagged"))
-          count += 1;
-        if (row < Driver.row - 1 && col > 0 && state[row + 1][col - 1].equals("flagged"))
-          count += 1;
-        if (col > 0 && state[row][col - 1].equals("flagged"))
-          count += 1;
-        if (count == key) {
-          boolean dead = false;
-          if (row > 0 && col > 0 && state[row - 1][col - 1].equals("covered"))
-            if (death(row - 1, col - 1) == true)
-              dead = true;
-          if (row > 0 && state[row - 1][col].equals("covered"))
-            if (death(row - 1, col) == true)
-              dead = true;
-          if (row > 0 && col < Driver.col - 1 && state[row - 1][col + 1].equals("covered"))
-            if (death(row - 1, col + 1) == true)
-              dead = true;
-          if (col < Driver.col - 1 && state[row][col + 1].equals("covered"))
-            if (death(row, col + 1) == true)
-              dead = true;
-          if (row < Driver.row - 1 && col < Driver.col - 1
-              && state[row + 1][col + 1].equals("covered"))
-            if (death(row + 1, col + 1) == true)
-              dead = true;
-          if (row < Driver.row - 1 && state[row + 1][col].equals("covered"))
-            if (death(row + 1, col) == true)
-              dead = true;
-          if (row < Driver.row - 1 && col > 0 && state[row + 1][col - 1].equals("covered"))
-            if (death(row + 1, col - 1) == true)
-              dead = true;
-          if (col > 0 && state[row][col - 1].equals("covered"))
-            if (death(row, col - 1) == true)
-              dead = true;
-          if (dead == true) {
-            for (int r = 0; r < Driver.row; r++) {
-              for (int c = 0; c < Driver.col; c++) {
-                if ((r >= row - 1 && r <= row + 1 && c >= col - 1 && c <= col + 1) == false &&
-                    state[r][c].equals("flagged") == false)
-                  state[r][c] = "uncovered";
+        for (int i = 0; i < 8; i++) {
+          if (neighbor[i][0] != -1 && state[neighbor[i][0]][neighbor[i][1]].equals("flagged")) {
+            count += 1;
+          }
+        }
+        if (count == this.key[row][col]) {
+          for (int i = 0; i < 8; i++) {
+            if (neighbor[i][0] != -1 && state[neighbor[i][0]][neighbor[i][1]].equals("covered")) {
+              state[neighbor[i][0]][neighbor[i][1]] = "uncovered";
+              if (key[neighbor[i][0]][neighbor[i][1]] == 9) {
+                gameOver = true;
               }
             }
-            if (row > 0 && col > 0)
-              deathSeq(row - 1, col - 1);
-            if (row > 0)
-              deathSeq(row - 1, col);
-            if (row > 0 && col < Driver.col - 1)
-              deathSeq(row - 1, col + 1);
-            if (col < Driver.col - 1)
-              deathSeq(row, col + 1);
-            if (row < Driver.row - 1 && col < Driver.col - 1)
-              deathSeq(row + 1, col + 1);
-            if (row < Driver.row - 1)
-              deathSeq(row + 1, col);
-            if (row < Driver.row - 1 && col > 0)
-              deathSeq(row + 1, col - 1);
-            if (col > 0)
-              deathSeq(row, col - 1);
+          }
+          if (gameOver == true) {
+            for (int r = 0; r < Driver.row; r++) {
+              for (int c = 0; c < Driver.col; c++) {
+                if (state[r][c].equals("covered")) {
+                  state[r][c] = "uncovered";
+                }
+                if (state[r][c].equals("flagged") && key[r][c] != 9) {
+                  state[r][c] = "badFlag";
+                }
+              }
+            }
+            for (int i = 0; i < 8; i++) {
+              if (state[neighbor[i][0]][neighbor[i][1]].equals("uncovered")
+                  && key[neighbor[i][0]][neighbor[i][1]] == 9) {
+                state[neighbor[i][0]][neighbor[i][1]] = "triggeredMine";
+              }
+            }
           }
         }
       }
@@ -255,47 +177,61 @@ public class Minesweeper {
   }
 
   /**
-   * checks if unflagged mine is uncovered, which causes death
+   * makes a 2d int array of every existing neighbor of the square. The upper left square is the
+   * first in sequence and one under it is the last, going clockwise. If the neighbor does not
+   * exist, its place would be held by -1
    * 
-   * @param row - the row index of the square in question
-   * 
-   * @param col - the column index of the square in question
-   * 
-   * @return true if the square in question is an unflagged mine, causing death, false if the square
-   * in question is not a mine or is a flagged mine, not fatal
+   * @param row - the row index of the central square
+   * @param col - the column index of the central square
+   * @return 2d int array of neighboring squares coordinates
    */
-  private boolean death(int row, int col) {
-    if (key[row][col] != 9) {
-      state[row][col] = "uncovered";
-      return false;
-    } else if (state[row][col].equals("flagged")) {
-      return false;
+  private int[][] neighbors(int row, int col) {
+    int[][] output = new int[8][2];
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 2; j++) {
+        output[i][j] = -1;
+      }
     }
-    return true;
+    if (row > 0 && col > 0) {
+      output[0][0] = row - 1;
+      output[0][1] = col - 1;
+    }
+    if (row > 0) {
+      output[1][0] = row - 1;
+      output[1][1] = col;
+    }
+    if (row > 0 && col < Driver.col - 1) {
+      output[2][0] = row - 1;
+      output[2][1] = col + 1;
+    }
+    if (col < Driver.col - 1) {
+      output[3][0] = row;
+      output[3][1] = col + 1;
+    }
+    if (row < Driver.row - 1 && col < Driver.col - 1) {
+      output[4][0] = row + 1;
+      output[4][1] = col + 1;
+    }
+    if (row < Driver.row - 1) {
+      output[5][0] = row + 1;
+      output[5][1] = col;
+    }
+    if (row < Driver.row - 1 && col > 0) {
+      output[6][0] = row + 1;
+      output[6][1] = col - 1;
+    }
+    if (col > 0) {
+      output[7][0] = row;
+      output[7][1] = col - 1;
+    }
+    return output;
   }
 
-  /**
-   * post mortem operations: change states of badFlags and triggeredMines to show cause of death
-   * 
-   * @param row - the row index of the square in question
-   * 
-   * @param col - the column index of the square in question
-   */
-  private void deathSeq(int row, int col) {
-    if (key[row][col] != 9 && state[row][col].equals("flagged"))
-      state[row][col] = "badFlag";
-    else if (key[row][col] == 9 && state[row][col].equals("flagged") == false)
-      state[row][col] = "triggeredMine";
-    else
-      state[row][col] = "uncovered";
-  }
-
-  /**
+  /*
    * continuously output the current game progress, executes user inputs based on the state of each
    * individual square
    */
   private void draw() {
-    // FIXME - lock down after victory/death
     for (int row = 0; row < Driver.row; row++) {
       for (int col = 0; col < Driver.col; col++) {
         if (state[row][col].equals("covered")) {

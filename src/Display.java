0e7c32abe685486e0d6b84babbb9510a7ee0a78a
laogao216 <57880087+laogao216@users.noreply.github.com>
2020-01-22 14:37:29 +0800

    v0.4
    -- organized references into the tile class, 
    -- used enumeration on the displayed state of the tile.

---
+++ src/Display.java
@@ -0,0 +1,10 @@
+///////////////////////////////////////// 100 COLUMNS WIDE /////////////////////////////////////////
+
+/**
+ * This Enumeration groups the name of constants representing the displayed states of tiles
+ *
+ * @author laogao216
+ */
+public enum Display {
+  UNCOVERED, COVERED, FLAG, BAD_FLAG, MINE, TRIGGERED_MINE;
+}

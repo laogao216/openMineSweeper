1d6d6f11661fbc4b7fe8a865b2909d4e233dbf73
laogao216 <57880087+laogao216@users.noreply.github.com>
2020-03-08 07:02:05 +0800

    v0.8
    -- add difficulty level selection with JOptionPanel
    -- enforce encapsulation by adding accessors to privatized public fields

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,7 @@
 ///////////////////////////////////////// 100 COLUMNS WIDE /////////////////////////////////////////
 
 import processing.core.PApplet;
+import javax.swing.JOptionPane;
 
 /**
  * This class is the driver application for Minesweeper.
@@ -9,20 +10,36 @@ import processing.core.PApplet;
  */
 public class Driver extends PApplet {
 
-  // TODO - prompt user for the following three fields
-  public static int row = 50;
-  public static int col = 50;
-  public static int mineCount = 500;
-
+  // TODO - add custom dimension, use jFrame or jPanel, throw error when mineCount > row * col - 9
+  private static int row;
+  private static int col;
+  private static int mineCount;
   private Minesweeper pen;
-  private int rowDisp = 0;
-  private int colDisp = 51;
-  // move the game board down or to the right. Unit is pixel.
 
   /**
    * runs PApplet
    */
   public static void main(String[] args) {
+    String[] options = {"Easy", "Normal", "Hard"};
+    String message = "Easy: 10*10, 10 mines" + System.lineSeparator() + "Normal: 20*20, 60 mines"
+        + System.lineSeparator() + "Hard: 50*50, 500 mines";
+    int input = JOptionPane.showOptionDialog(null, message, "choose difficulty level",
+        JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+    if (input == 0) {
+      row = 10;
+      col = 10;
+      mineCount = 10;
+    } else if (input == 1) {
+      row = 20;
+      col = 20;
+      mineCount = 60;
+    } else if (input == 2) {
+      row = 50;
+      col = 50;
+      mineCount = 500;
+    } else {
+      return;
+    }
     PApplet.main("Driver");
   }
 
@@ -33,7 +50,7 @@ public class Driver extends PApplet {
    */
   @Override
   public void settings() {
-    size(row * 16 + 1 + rowDisp, col * 16 + 1 + colDisp);
+    size(row * 16 + 1, col * 16 + 1 + 51);
   }
 
   /**
@@ -43,7 +60,7 @@ public class Driver extends PApplet {
    */
   @Override
   public void setup() {
-    pen = new Minesweeper(this, rowDisp, colDisp);
+    pen = new Minesweeper(this);
   }
 
   /**
@@ -54,6 +71,33 @@ public class Driver extends PApplet {
   @Override
   public void draw() {
     pen.update(mouseX, mouseY, mousePressed, mouseButton);
+  }
+
+  /**
+   * accessor for row
+   * 
+   * @return length of rows of tiles
+   */
+  public static int getRow() {
+    return row;
+  }
+
+  /**
+   * accessor for col
+   * 
+   * @return length of columns of tiles
+   */
+  public static int getCol() {
+    return col;
+  }
+
+  /**
+   * accessor for mineCount
+   * 
+   * @return number of mines
+   */
+  public static int getMineCount() {
+    return mineCount;
   }
 
 }
